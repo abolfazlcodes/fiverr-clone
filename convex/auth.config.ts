8db48b373736731c3da4a1d702fc48d4054c1980
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-23 22:16:30 +0330

    setup shadnci/ui components, auth and database with Clerk and convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: "https://close-man-13.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
