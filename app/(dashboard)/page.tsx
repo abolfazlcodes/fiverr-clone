97ce1ae357ff56951ffbc03412513621ba9676aa
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-26 21:44:09 +0330

    add store user function to db

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useMutation } from "convex/react";
+import { useEffect } from "react";
+
 // import { GigList } from "./_components/gig-list";
 
 interface IDashboardProps {
@@ -11,6 +14,16 @@ interface IDashboardProps {
 }
 
 const Dashboard = ({ searchParams }: IDashboardProps) => {
+  const store = useMutation(api.users.store);
+
+  useEffect(() => {
+    const storeUser = async () => {
+      await store({});
+    };
+
+    storeUser();
+  }, [store]);
+
   return (
     // <GigList query={searchParams} />
 
